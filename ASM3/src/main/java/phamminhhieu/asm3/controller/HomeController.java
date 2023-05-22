02f0497750d3e2fb4911689f9339fd219f153162
minhhieupham2002 <minhhieupham2002@gmail.com>
2023-05-22 21:02:00 +0700

    ASM5

--- ASM3/src/main/java/phamminhhieu/asm3/controller/HomeController.java
+++ ASM3/src/main/java/phamminhhieu/asm3/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import phamminhhieu.asm3.service.CourseService;
 
 @Controller
@@ -14,8 +15,10 @@ public class HomeController {
     private CourseService courseService;
 
     @GetMapping("/test")
+    @ResponseBody
     public String index() {
-        return "home";
+        String s = "5";
+        return 2 + 20 + s + 20 +2 ;
     }
 
     @GetMapping("")
